4bbc7fe68afce4c292bc7299930888ab41e199b7
Tyler Matrix <tylermatrix0@gmail.com>
2017-03-20 19:57:36 -0500

    init commit

---
+++ constructor.js
@@ -0,0 +1,17 @@
+//Essential Design Pattern Book
+// Dot syntax
+
+
+//Set properties
+newObject.someKey = "Hello World";
+
+// Get properties
+var value = newObject.someKey;
+
+//2. Square bracket syntax
+
+//Get properties
+newObject["someKey"] = "Hello World";
+
+//Get Properties
+var value = newObject["someKey"];
